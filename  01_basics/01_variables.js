57832b433330f6af4388ff881d876d671cd898de
codebyshivamsahu <163376282+codebyshivamsahu@users.noreply.github.com>
2024-09-04 10:28:47 +0000

    datatyps

---
+++  01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =77598989;
+let accountEmail = "ershivamsahucs@gmail.com";
+var accountPasword ="010101";
+accountCity = "Bhagalpur";
+let accountState;
+//accountId = 707070;
+
+console.log(accountId,accountEmail,accountPasword,accountCity,accountState);
+console.table([accountId,accountEmail,accountPasword,accountCity,accountState]);
+
