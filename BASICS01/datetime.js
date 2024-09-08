7e7abea9106dc6b5acb018c00421de9e951dea0b
codebyshivamsahu <163376282+codebyshivamsahu@users.noreply.github.com>
2024-09-08 17:16:20 +0000

    name modify

--- BASICS01/datetime.js
+++ BASICS01/datetime.js
@@ -10,5 +10,8 @@ console.log(typeof myDate);
 let myCreateDate = new Date(2024,9,7)
 //let myCreateDate = new Date(2024,9,7)
 //let myCreateDate = new Date("07-09-2024");
+let myCreatedDate = new Date("08-09-2024")
+console.log(myCreatedDate.toLocaleDateString);
+
 
 
